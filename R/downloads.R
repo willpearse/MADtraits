0a56abd613c780fbce62a1a985121051764c0110
Will Pearse <will.pearse@gmail.com>
2017-02-21 08:50:48 -0700

    Cleanup before meeting

--- R/downloads.R
+++ R/downloads.R
@@ -9,7 +9,7 @@
 # -- this last import must be removed, because testdat isn't on cran (devtools::install_github("ropensci/testdat"))
 
 #Deleted Kew because needs download (?...?)
-.wright.2004.glop <- function(...){
+.wright.2004 <- function(...){
     raw <- read.xls("http://www.nature.com/nature/journal/v428/n6985/extref/nature02403-s2.xls", as.is=TRUE, skip=7)
     raw$metadata <- with(raw, paste(Dataset,BIOME,sep="_"))
     raw <- raw[,!names(raw) %in% c("Code","Dataset","BIOME","X","X.1","X.2","X.3","X.4","X.5","X.6")]
@@ -18,7 +18,7 @@
     class(output) <- "natdb"
     return(output)
 }
-.zanne.2014.keys <- function(...){
+.zanne.2014 <- function(...){
     wood <- read.csv("http://datadryad.org/bitstream/handle/10255/dryad.59002/GlobalWoodinessDatabase.csv?sequence=1")
     names(wood)[3] <- "metadata"
     phenol <- read.csv("http://datadryad.org/bitstream/handle/10255/dryad.59005/GlobalLeafPhenologyDatabase.csv?sequence=1")
@@ -26,14 +26,14 @@
     output$gs <- gsub(" ", "_", tolower(output$gs))
     return(.df.melt(output, "gs"))
 }
-.hintze.2013.seeds <- function(...){
+.hintze.2013 <- function(...){
     data <- read.csv("http://www.sciencedirect.com/science/MiamiMultiMediaURL/1-s2.0-S1433831913000218/1-s2.0-S1433831913000218-mmc1.txt/273233/html/S1433831913000218/6bd947d6c0ccb7edd11cd8bf73648447/mmc1.txt", sep=";", as.is=TRUE)
     data$metadata <- seq_len(nrow(data))
     data$name <- sapply(strsplit(tolower(sanitize_text(data$name)),split=" "), function(x) paste(x[1:2], collapse="_"))
     data <- data[,!names(data) %in% c("comment","family","citation_total","citation_prop_ane","citation_prop_dyso","citation_prop_endo","citation_prop_epi","citation_prop_hem","citation_prop_hydro","citation_prop_other")]
     return(.df.melt(data, "name"))
 }
-.bezeng.2015.darwin <- function(...){
+.bezeng.2015 <- function(...){
     data <- read.xls("http://datadryad.org/bitstream/handle/10255/dryad.84999/Table%20S2.xls?sequence=1", as.is=TRUE)
     data$metadata <- sapply(strsplit(data$Species, "_"), function(x) x[3])
     data$Species <- sapply(strsplit(data$Species, "_"), function(x) paste(x, collapse="_"))
--- R/utility.R
+++ R/utility.R
@@ -10,11 +10,12 @@
 #'     named paste(zip,file, sep='_'))
 #' @return Complete path to unzipped file
 #' @importFrom utils unzip
+#' @importFrom reshape2 melt
 .unzip <- function(file, zip, to.save.dir, to.save.name){
     if(missing(to.save.dir))
         to.save.dir <- tempdir()
     if(missing(to.save.name))
-        to.save.name <- paste(file, zip, sep='_')
+        to.save.name <- file
     
     files <- unzip(zip, list=TRUE)
     if(!file %in% files$Name)
@@ -37,18 +38,69 @@
     colnames(output) <- paste(names, gsub("factor_to_expand", "", colnames(output)), sep="_")
     return(as.data.frame(output))
 }
-.df.melt <- function(x, species){
+.df.melt <- function(x, species, units){
+    if(!"metadata" %in% names(x))
+        x$metadata <- NA
+    if(missing(units)){
+        units <- setNames(rep(NA, length(names(x))), names(x))
+    } else {
+        units <- setNames(units, setdiff(names(x),c(species,"metadata")))
+    }
     numeric <- x[,sapply(x, is.numeric) | names(x) %in% c(species,"metadata")]
     if(ncol(numeric) > 2){
         numeric <- melt(numeric, id.vars=c(species,"metadata"))
         numeric <- numeric[!is.na(numeric$value),]
         names(numeric)[1] <- "species"
+        numeric$units <- units[numeric$variable]
     } else numeric <- NULL
     character <- x[,sapply(x, Negate(is.numeric)) | names(x) %in% c(species,"metadata")]
     if(ncol(character) > 2){
         character <- melt(character, id.vars=c(species,"metadata"))
         character <- character[!is.na(character$value),]
         names(character)[1] <- "species"
+        character$units <- units[character$variable]
     } else character <- NULL
     return(list(numeric=numeric,character=character))
+}
+.download <- function(url, dir, save.name, cache=TRUE){
+    destination <- file.path(dir, save.name)
+    suffix <- .file.suffix(url, 4)
+    
+    if(cache==TRUE & file.exists(destination)){
+        if(!is.na(suffix))
+            attr(destination, "suffix") <- suffix
+        return(destination)
+    }
+    
+    result <- download.file(url, destination, quiet=TRUE)
+    if(result != 0)
+        stop("Error code", result, " downloading file; file may not exist")
+    
+    if(!is.na(suffix))
+        attr(destination, "suffix") <- suffix
+    return(destination)
+}
+.save.name <- function(doi, save.name, file){
+    if(is.na(save.name)){
+        save.name <- paste(doi,file, sep="_")
+        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
+    }
+    return(save.name)
+}
+.grep.url <- function(url, regexp, which=1){
+    html <- as.character(GET(url))
+    return(.grep.text(html, regexp, which))
+}
+.grep.text <- function(text, regexp, which=1){
+    links <- gregexpr(regexp, text)
+    if(which > length(links[[1]]))
+        stop("SI number '", which, "' greater than number of detected SIs (", length(links[[1]]), ")")
+    pos <- as.numeric(links[[1]][which])
+    return(substr(text, pos, pos+attr(links[[1]], "match.length")[which]-1))
+}
+.file.suffix <- function(text, max.length=4){
+    suffix <- .grep.text(text, "[a-zA-Z]+$")
+    if(nchar(suffix) <= max.length & nchar(suffix) > 0)
+        return(suffix)
+    return(NA)
 }
