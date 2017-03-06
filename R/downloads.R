7be3f89cc3ecb341df73c6580b758122b2139d4c
Konrad Hafen <khafen74@gmail.com>
2017-03-06 16:17:48 -0700

    added cariveau 2016

--- R/downloads.R
+++ R/downloads.R
@@ -39,3 +39,14 @@
     data$Species <- sapply(strsplit(data$Species, "_"), function(x) paste(x, collapse="_"))
     return(.df.melt(data, "Species"))
 }
+.cariveau.2016 <- function(...){
+  
+  link = "http://journals.plos.org/plosone/article/file?type=supplementary&id=info:doi/10.1371/journal.pone.0151482.s003"
+  file = "C:/crap/journal.pone.0151482.s003.XLSX"
+  sheet = "TableS1_v2"
+  data = read.xlsx(ft_get_si("10.1371/journal.pone.0151482", 3), sheetName=sheet)
+
+  data = .df.melt(data, "species", c(NA, NA, NA, NA, NA, "#", "#", "#", "#", "mm","mm","mm","mm",NA,NA))
+  
+  return(data)
+}
