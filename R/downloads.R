8da7d3ba4a0a52aec3d8c4fd96aea712f897c97b
Nicholas Huron <nahuron@gmail.com>
2018-04-23 21:17:24 -0400

    added zagar 2017 fnx

--- R/downloads.R
+++ R/downloads.R
@@ -1350,3 +1350,28 @@
     class(output) <- "natdb"
     return(output)
 }
+
+##########New functions added for papers by Temple folks
+
+#' @export
+.zagar.2017 <- function(...){
+  #fetch original dataset
+  data <- read.csv(suppdata(x = "10.5061/dryad.jn14f", si = "data.csv"), as.is = TRUE)
+  #change the species labels to natdb format
+  data$SP <- gsub(pattern = "IHOR", replacement = "iberolacerta_horvathi", x = data$SP)
+  data$SP <- gsub(pattern = "PMUR", replacement = "podarcis_muralis", x = data$SP)
+  #rename species col
+  colnames(data)[colnames(data) %in% "SP"] <- "species"
+  #separate out metadata
+  metadata <- as.data.frame(data[,2:5])
+  colnames(metadata) <- c("sex", "max_bite_force", "max_run_speed", "max_climb_speed")
+  #separate out focal data
+  data <- as.data.frame(data[,-2:-5])
+  colnames(data) <- c("species", "snout_vent_length", "trunk_length", "head_length", "pileus_length", "head_width", "head_height", "mouth_opening", "fore_limb_length", "hind_limb_length")
+  #create units object
+  units <- c(rep("mm", times = 9))
+  #summary dataframe
+  data <- .df.melt(x = data, spp = "species", units = units, metadata = metadata)  
+  #report out results
+  return(data)
+}
